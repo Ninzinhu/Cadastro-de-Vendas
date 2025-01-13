3df9e592b60012af54b9debfc8b0ca0934cd6eae
João Victor Emmanuell <pel118201@gmail.com>
2025-01-13 03:44:30 -0300

    🚑 Fix: Dotenv

--- finance-control/server.js
+++ finance-control/server.js
@@ -9,11 +9,10 @@ const port = 3000;
 
 // Inicializa o Firebase Admin SDK
 require('dotenv').config();
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY); 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://financecontrol-c2228-default-rtdb.firebaseio.com/' 
-});
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount), databaseURL: 'https://financecontrol-c2228-default-rtdb.firebaseio.com/' });
+
+
 
 const db = admin.database();
 
